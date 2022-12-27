52a85db06a8faed446f04ea23d8a1fb400da0660
Thomas Kranitsas <thomas.kranitsas@topcoder.com>
2022-12-27 14:24:22 +0200

    implement attachment, challenge type, challenge track, phase, and timeline template

--- domains/domain-challenge/src/index.ts
+++ domains/domain-challenge/src/index.ts
@@ -1,1 +1,6 @@
+export * from "./ChallengeTrack";
+export * from "./ChallengeType";
+export * from "./Attachment";
 export * from "./Challenge";
+export * from "./Phase";
+export * from "./TimelineTemplate";
