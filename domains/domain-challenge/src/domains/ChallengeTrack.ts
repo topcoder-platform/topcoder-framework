e541a4c3d18b34e452be67b37d95b6dd99de96ac
Rakib Ansary <rakibansary@gmail.com>
2022-12-31 03:53:19 +0600

    fix: incorrect base_url

--- domains/domain-challenge/src/domains/ChallengeTrack.ts
+++ domains/domain-challenge/src/domains/ChallengeTrack.ts
@@ -1,65 +1,65 @@
-// import { Metadata } from "@grpc/grpc-js";
+import { Metadata } from "@grpc/grpc-js";
 
-// import { GrpcClient } from "../common/GrpcClient";
-// import { ChallengeTrackClient } from "../models/domain-layer/challenge/services/challenge_track";
-// import { LookupCriteria, ScanRequest } from "../models/common/common";
+import { GrpcClient } from "../common/GrpcClient";
+import { ChallengeTrackClient } from "../models/domain-layer/challenge/services/challenge_track";
+import { LookupCriteria, ScanRequest } from "../models/common/common";
 
-// import {
-//   CreateChallengeTrackInput,
-//   UpdateChallengeTrackInput,
-// } from "../models/domain-layer/challenge/challenge_track";
+import {
+  CreateChallengeTrackInput,
+  UpdateChallengeTrackInput,
+} from "../models/domain-layer/challenge/challenge_track";
 
-// export class ChallengeTrack extends GrpcClient<ChallengeTrackClient> {
-//   public async create(
-//     param: CreateChallengeTrackInput,
-//     metadata: Metadata = new Metadata()
-//   ) {
-//     return new Promise((resolve, reject) => {
-//       this.client.create(param, metadata, (error, response) => {
-//         if (error) reject(error);
-//         else resolve(response);
-//       });
-//     });
-//   }
-//   public async scan(param: ScanRequest, metadata: Metadata = new Metadata()) {
-//     return new Promise((resolve, reject) => {
-//       this.client.scan(param, metadata, (error, response) => {
-//         if (error) reject(error);
-//         else resolve(response);
-//       });
-//     });
-//   }
-//   public async lookup(
-//     param: LookupCriteria,
-//     metadata: Metadata = new Metadata()
-//   ) {
-//     return new Promise((resolve, reject) => {
-//       this.client.lookup(param, metadata, (error, response) => {
-//         if (error) reject(error);
-//         else resolve(response);
-//       });
-//     });
-//   }
-//   public async update(
-//     param: UpdateChallengeTrackInput,
-//     metadata: Metadata = new Metadata()
-//   ) {
-//     return new Promise((resolve, reject) => {
-//       this.client.update(param, metadata, (error, response) => {
-//         if (error) reject(error);
-//         else resolve(response);
-//       });
-//     });
-//   }
-//   public async delete(
-//     param: LookupCriteria,
-//     metadata: Metadata = new Metadata()
-//   ) {
-//     return new Promise((resolve, reject) => {
-//       this.client.delete(param, metadata, (error, response) => {
-//         if (error) reject(error);
-//         else resolve(response);
-//       });
-//     });
-//   }
-// }
+export class ChallengeTrack extends GrpcClient<ChallengeTrackClient> {
+  public async create(
+    param: CreateChallengeTrackInput,
+    metadata: Metadata = new Metadata()
+  ) {
+    return new Promise((resolve, reject) => {
+      this.client.create(param, metadata, (error, response) => {
+        if (error) reject(error);
+        else resolve(response);
+      });
+    });
+  }
+  public async scan(param: ScanRequest, metadata: Metadata = new Metadata()) {
+    return new Promise((resolve, reject) => {
+      this.client.scan(param, metadata, (error, response) => {
+        if (error) reject(error);
+        else resolve(response);
+      });
+    });
+  }
+  public async lookup(
+    param: LookupCriteria,
+    metadata: Metadata = new Metadata()
+  ) {
+    return new Promise((resolve, reject) => {
+      this.client.lookup(param, metadata, (error, response) => {
+        if (error) reject(error);
+        else resolve(response);
+      });
+    });
+  }
+  public async update(
+    param: UpdateChallengeTrackInput,
+    metadata: Metadata = new Metadata()
+  ) {
+    return new Promise((resolve, reject) => {
+      this.client.update(param, metadata, (error, response) => {
+        if (error) reject(error);
+        else resolve(response);
+      });
+    });
+  }
+  public async delete(
+    param: LookupCriteria,
+    metadata: Metadata = new Metadata()
+  ) {
+    return new Promise((resolve, reject) => {
+      this.client.delete(param, metadata, (error, response) => {
+        if (error) reject(error);
+        else resolve(response);
+      });
+    });
+  }
+}
--- domains/domain-challenge/src/domains/TimelineTemplate.ts
+++ domains/domain-challenge/src/domains/TimelineTemplate.ts
@@ -1,71 +1,71 @@
-// import { Metadata, ServiceError } from "@grpc/grpc-js";
-// import { TimelineTemplateClient } from "../models/domain-layer/challenge/services/timeline_template";
-// import {
-//   CreateTimelineTemplateInput,
-//   UpdateTimelineTemplateInput,
-// } from "../models/domain-layer/challenge/timeline_template";
-// import { GrpcClient } from "../common/GrpcClient";
-// import { LookupCriteria, ScanRequest } from "../models/common/common";
+import { Metadata, ServiceError } from "@grpc/grpc-js";
+import { TimelineTemplateClient } from "../models/domain-layer/challenge/services/timeline_template";
+import {
+  CreateTimelineTemplateInput,
+  UpdateTimelineTemplateInput,
+} from "../models/domain-layer/challenge/timeline_template";
+import { GrpcClient } from "../common/GrpcClient";
+import { LookupCriteria, ScanRequest } from "../models/common/common";
 
-// export class TimelineTemplate extends GrpcClient<TimelineTemplateClient> {
-//   public async create(
-//     param: CreateTimelineTemplateInput,
-//     metadata: Metadata = new Metadata()
-//   ) {
-//     return new Promise((resolve, reject) => {
-//       this.client.create(param, metadata, (error, response) => {
-//         if (error) reject(error);
-//         else resolve(response);
-//       });
-//     });
-//   }
+export class TimelineTemplate extends GrpcClient<TimelineTemplateClient> {
+  public async create(
+    param: CreateTimelineTemplateInput,
+    metadata: Metadata = new Metadata()
+  ) {
+    return new Promise((resolve, reject) => {
+      this.client.create(param, metadata, (error, response) => {
+        if (error) reject(error);
+        else resolve(response);
+      });
+    });
+  }
 
-//   public async scan(param: ScanRequest, metadata: Metadata = new Metadata()) {
-//     return new Promise((resolve, reject) => {
-//       this.client.scan(param, metadata, (error, response) => {
-//         if (error) reject(error);
-//         else resolve(response);
-//       });
-//     });
-//   }
+  public async scan(param: ScanRequest, metadata: Metadata = new Metadata()) {
+    return new Promise((resolve, reject) => {
+      this.client.scan(param, metadata, (error, response) => {
+        if (error) reject(error);
+        else resolve(response);
+      });
+    });
+  }
 
-//   public async lookup(
-//     param: LookupCriteria,
-//     metadata: Metadata = new Metadata()
-//   ) {
-//     return new Promise((resolve, reject) => {
-//       this.client.lookup(param, metadata, (error, response) => {
-//         if (error) reject(error);
-//         else resolve(response);
-//       });
-//     });
-//   }
+  public async lookup(
+    param: LookupCriteria,
+    metadata: Metadata = new Metadata()
+  ) {
+    return new Promise((resolve, reject) => {
+      this.client.lookup(param, metadata, (error, response) => {
+        if (error) reject(error);
+        else resolve(response);
+      });
+    });
+  }
 
-//   public async update(
-//     param: UpdateTimelineTemplateInput,
-//     metadata: Metadata = new Metadata()
-//   ) {
-//     return new Promise((resolve, reject) => {
-//       this.client.update(
-//         param,
-//         metadata,
-//         (error: ServiceError | null, response) => {
-//           if (error) reject(error);
-//           else resolve(response);
-//         }
-//       );
-//     });
-//   }
+  public async update(
+    param: UpdateTimelineTemplateInput,
+    metadata: Metadata = new Metadata()
+  ) {
+    return new Promise((resolve, reject) => {
+      this.client.update(
+        param,
+        metadata,
+        (error: ServiceError | null, response) => {
+          if (error) reject(error);
+          else resolve(response);
+        }
+      );
+    });
+  }
 
-//   public async delete(
-//     param: LookupCriteria,
-//     metadata: Metadata = new Metadata()
-//   ) {
-//     return new Promise((resolve, reject) => {
-//       this.client.delete(param, metadata, (error, response) => {
-//         if (error) reject(error);
-//         else resolve(response);
-//       });
-//     });
-//   }
-// }
+  public async delete(
+    param: LookupCriteria,
+    metadata: Metadata = new Metadata()
+  ) {
+    return new Promise((resolve, reject) => {
+      this.client.delete(param, metadata, (error, response) => {
+        if (error) reject(error);
+        else resolve(response);
+      });
+    });
+  }
+}
