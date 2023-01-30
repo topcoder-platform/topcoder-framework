8de741a157c227928b47d227013a1993c4bac20d
Rakib Ansary <rakibansary@topcoder.com>
2023-01-31 04:17:14 +0600

    feat(lib-common): add well known type empty

--- lib/lib-common/src/index.ts
+++ lib/lib-common/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./models/common/common";
+export * from "./models/google/protobuf/empty";
 export * from "./models/google/protobuf/struct";
-export * from "./util/DomainHelper";
 export * from "./models/google/protobuf/timestamp";
+export * from "./util/DomainHelper";
