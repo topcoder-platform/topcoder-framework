44ddd416e64a1cb19c8f6c161298086680cde733
Rakib Ansary <rakibansary@topcoder.com>
2023-02-24 01:40:55 +0600

    feat(lib-common): add value helper

---
+++ lib/lib-common/src/util/ValueHelper.ts
@@ -0,0 +1,7 @@
+import { Value } from "../models/google/protobuf/struct";
+
+export class ValueHelper {
+  public static isValue(x: unknown): x is Value {
+    return typeof x === "object" && x !== null && "$case" in x;
+  }
+}
